dfe8f0b17d2c013f13a66244892f94a7ffc9eace
qiyu-lu <1186387362@qq.com>
2025-12-04 22:51:06 +0800

    438_固定字母异位词固定窗口滑动

---
+++ src/main/java/com/leetcode/hot100/slidingWindow/_438_findAnagrams.java
@@ -0,0 +1,69 @@
+package com.leetcode.hot100.slidingWindow;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class _438_findAnagrams {
+    /**
+     * 给定两个字符串 s 和 p，
+     * 找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+     * */
+    public List<Integer> findAnagramsBF(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int n = p.length();
+        char[] pChars = p.toCharArray();
+        Arrays.sort(pChars);
+        for(int i = 0; i < s.length(); ++i){
+            if(i + n > s.length()) return ans;
+            char[] temp = s.substring(i, i + n).toCharArray();
+            Arrays.sort(temp);
+            if(Arrays.equals(temp, pChars)){ans.add(i);}
+        }
+        return ans;
+    }
+    public List<Integer> findAnagramsBySw(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int n = p.length();
+        int [] cnt = new int[26];//用来进行标记
+        for(int i = 0; i < n; ++i){
+            cnt[p.charAt(i) - 'a']++;
+        }
+        int left = 0;
+        int[] cntTemp = new int[26];
+        int right = 0;
+        for(; right < s.length();right++){
+            while(right - left >= n){
+                if(right - left == n && Arrays.equals(cnt, cntTemp) )//相等的话
+                    ans.add(left);
+                cntTemp[s.charAt(left) - 'a']--;//然后更新窗口
+                left++;//无论是否是异位词子串的话都缩小窗口
+            }
+            cntTemp[s.charAt(right) - 'a']++;
+        }
+        if(right - left == n && Arrays.equals(cnt, cntTemp) )//最后到最后了还有一组没有判断
+            ans.add(left);
+        return ans;
+    }
+    public List<Integer> findAnagramsBySwV2(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        int n = p.length();
+        int [] cnt = new int[26];//用来进行标记
+        for(int i = 0; i < n; ++i){
+            cnt[p.charAt(i) - 'a']++;
+        }
+        int left = 0;
+        int[] cntTemp = new int[26];
+        for(int right = 0; right < s.length();right++){
+            cntTemp[s.charAt(right) - 'a']++;
+            while (right - left + 1 >= n){
+                if(right - left + 1 == n && Arrays.equals(cnt, cntTemp) )//相等的话
+                    ans.add(left);
+                cntTemp[s.charAt(left) - 'a']--;//然后更新窗口
+                left++;//无论是否是异位词子串的话都缩小窗口
+            }
+        }
+        return ans;
+    }
+}
