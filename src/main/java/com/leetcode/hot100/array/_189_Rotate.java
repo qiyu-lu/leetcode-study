9f6f66d23e38dcb1d751c462c374fb95a19e85ad
qiyu <1186387362@qq.com>
2025-12-19 23:31:01 +0800

    轮转数组

---
+++ src/main/java/com/leetcode/hot100/array/_189_Rotate.java
@@ -0,0 +1,33 @@
+package com.leetcode.hot100.array;
+
+public class _189_Rotate {
+    /**
+     * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+     * 示例 1:
+     * 输入: nums = [1,2,3,4,5,6,7], k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右轮转 1 步: [7,1,2,3,4,5,6]
+     * 向右轮转 2 步: [6,7,1,2,3,4,5]
+     * 向右轮转 3 步: [5,6,7,1,2,3,4]
+     *
+     *
+     * 将数组分为两个部分，先全部翻转，然后将两个部分分别反转
+     * * */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    private void reverse(int[] nums, int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
